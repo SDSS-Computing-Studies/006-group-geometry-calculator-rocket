32ca15a1aaabd2f71827335ef8d9cf596e4d95be
Lj <55156461+Usercodehappen@users.noreply.github.com>
2021-03-15 12:59:07 -0700

    update

--- assignment.py
+++ assignment.py
@@ -7,14 +7,14 @@ def title():
     # Will display a title screen
     # input parameters: none needed
     # output parameters: None
-    # Author: Copper
-    # Modified:
+    # Author: Lj
+    # Modified: Lj fernandez
     # title
-    print("========================")
-    print("========================")
-    print("Welcome to the Calculator")
-    print("========================")
-    print("========================")
+    print("=======================")
+    print("=======================")
+    print("===Volume calculator===")
+    print("=======================")
+    print("=======================")
     e=input("Press The Enter Key to Begin")
 
     return None
@@ -23,36 +23,80 @@ def instructions():
     # Will display instructions
     # input parameters: none needed
     # output parameters: None
-    # Author:
-    # Modified:
+    # Author: Lj
+    # Modified: Lj Fernandez
     print("choose any shape from this category: Triangle, Rectangle, Circle, Square")
     a=input(">>>")
     pass
 
-def getParams(shape):
-    # Will create a list of questions to be asked depending on the shape.
-    # These will be asked so that the user can enter in appropriate values
-    # input parameter: string 
-    # output parameter: return a list containing the prompts for each shape:
-    # example: ["Enter the radius:","Enter the slant height:","Enter the height:"]
-    prompts
+def squareArea():
+    print("Type any number for 1")
+    t=input(">>>")
+    t=int(t)
+    t=(t**2)
+    print (t)
 
-    return prompts
+def circleArea():
+    print("Type any Number for 1")
+    t=input(">>>")
+    t=int(t)
+    t=(math.pi * t**2)
+    print(t)
 
-def getInputs(questions):
-    # Will prompt the user for inputs for the shape they.
-    # These will be asked so that the user can enter in appropriate values
-    # It will turn all the input data into a list
-    # input parameter: list containing the prompts/questions
-    # output parameter: return a list containing all the measurements of the shape
-    measurements
-    
-    return measurements
+def triangleArea():
+    print("Type any Number for 1")
+    t=input(">>>")
+    print("Type any Number for 2")
+    f=input(">>>")
+    t=int(t)
+    f=int(f)
+    t=(t*f/2)
+    print(t)
+
+def rectangleArea():
+    print("Type any Number for 1")
+    t=input(">>>")
+    print("Type any Number for 2")
+    f=input(">>>")
+    t=int(t)
+    f=int(f)
+    t=(t*f)
+    print(t)
 
 def main():
     # main block of code that will run your program and control program flow
     # You will need to include a while loop to keep repeating the commands until
     # the user chooses to exit
-    title()
+
+     quit = False  
+      title()
+
+    while quit == False:
+        instructions()
+        a=input(">>> ")
+        if a =="Square":
+            squareArea()
+        elif a =="Circle":
+            circleArea()
+        elif a =="Triangle":
+            triangleArea()
+        elif a =="Rectangle":
+            rectangleArea()
+        else:
+            print ("Invaild, Choose a Shape!")
+        
+        if a == "q":
+            print("Are You Sure to Quit?\n Press C to Continue\n Press Q to Quit")
+            menu=input(">>>")
+            if menu == "c""C":
+                instructions()
+            elif menu == "q""Q":
+                print ("later!!!")
+                quit == True
+            break
 
 main()
+
+
+
+
